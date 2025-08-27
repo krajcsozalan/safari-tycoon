5d861646291dcd5f6430c393af208219758c765c
Krajcsovicz Zalán <184699004+krajcsozalan@users.noreply.github.com>
2025-08-27 14:52:11 +0200

    feat(controller/world): add worldcontroller

---
+++ Assets/Scripts/Controller/World/WorldController.cs
@@ -0,0 +1,46 @@
+/*
+ * safari-tycoon - An open-source tycoon game made with Unity
+ * Copyright (C) 2025  krajcsozalan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+using UnityEngine;
+
+using SafariTycoon.Model;
+
+namespace SafariTycoon.Controller
+{
+	public class WorldController : MonoBehaviour
+	{
+		public World World { get; private set; }
+
+		public void Initialize(uint worldSize, uint chunkSize)
+		{
+			World = new World(worldSize, chunkSize);
+		}
+
+		public void Generate()
+		{
+			World.Generate(new WorldGenerator());
+		}
+
+		private class WorldGenerator : IWorldGenerator
+		{
+			public float GetHeight(uint x, uint z)
+			{
+				return 0f;
+			}
+		};
+	}
+}
